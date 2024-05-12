307939adaef351ed042d858f7e318a961e943415
Volodymyr Popov <texnolife@gmail.com>
2024-05-12 23:38:41 +0100

    add Admin CRUD for Services

--- service/urls.py
+++ service/urls.py
@@ -5,4 +5,7 @@ app_name = "service"
 
 urlpatterns = [
     path("", views.service, name="services"),
+    path('create-service', views.create_service, name='create-service'),
+    path('update-service/<str:pk>', views.update_service, name='update-service'),
+    path('delete-service/<str:pk>', views.delete_service, name='delete-service'),
 ]
--- service/views.py
+++ service/views.py
@@ -1,14 +1,77 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from service.forms import ServiceForm
 from service.models import Service
 
 # Create your views here.
 def service(request):
 
-    services = Service.objects.all()
+    services = Service.objects.all().order_by('id')
     context = {
         'title': "Services",
         'services': services,
     }
 
     return render(request, "service/service.html",context)
+
+@login_required(login_url="my-login")
+def create_service(request):
+    if not request.user.is_superuser:
+        return redirect("service:services")
+
+    if request.method == "POST":
+        form = ServiceForm(request.POST)
+        if form.is_valid():
+            serv = form.save(commit=False)
+            serv.user = request.user
+            serv.save()
+            return redirect("service:services")
+    else:
+        form = ServiceForm()
+
+    context = {
+        "CreateServiсeForm": form,
+    }
+    return render(request, "service/create-service.html", context)
+
+@login_required(login_url="my-login")
+def update_service(request, pk):
+    if not request.user.is_superuser:
+        return redirect("service:services")
+    
+    try:
+        serv = Service.objects.get(id=pk)
+
+    except Service.DoesNotExist:
+        return redirect("service:services")
+
+    form = ServiceForm(instance=serv)
+
+    if request.method == "POST":
+        form = ServiceForm(request.POST, instance=serv)
+        if form.is_valid():
+            form.save()
+            return redirect("service:services")
+    context = {
+        "UpdateServiсeForm": form,
+    }
+
+    return render(request, "service/update-service.html", context)
+
+
+@login_required(login_url="my-login")
+def delete_service(request, pk):
+    if not request.user.is_superuser:
+        return redirect("service:services")
+    
+    try:
+        serv = Service.objects.get(id=pk)
+
+    except Service.DoesNotExist:
+        return redirect("service:services")
+
+    if request.method == "POST":
+        serv.delete()
+        return redirect("service:services")
+
+    return render(request, "service/delete-service.html")
